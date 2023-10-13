11c32d8fba787a79515ad928d4cc98a328f9310a
rojinpra7 <89182903+rojinpra7@users.noreply.github.com>
2023-10-13 11:07:55 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Rojin Prajapati on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
